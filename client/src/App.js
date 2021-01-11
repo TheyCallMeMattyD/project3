349ae98bad0a06f803ba8b68e4531fe7e20e08e3
chefbrams <oldcitycatering@gmail.com>
2021-01-10 19:50:32 -0500

    login and signup components

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import FavoritesList from "./pages/FavoritesList";
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
 
 function App() {
   return (
@@ -15,7 +17,10 @@ function App() {
           <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
+
             <Route exact path="/home" component={Home} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
             <Route exact path="/favorites" component={FavoritesList} />
             <Route exact path="/posts/:id" component={Detail} />
             <Route component={NoMatch} />
