c6fd03ea37205464877a02b479f40d682cb3f67f
chefbrams <oldcitycatering@gmail.com>
2021-01-11 14:38:04 -0500

    memeber schema, crud, api routes, added signup to global state

--- client/src/components/signup.component.js
+++ client/src/components/signup.component.js
@@ -1,36 +1,75 @@
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
+import Login from "./login.component";
+import { useStoreContext } from "../utils/GlobalState";
+import { ADD_POST, LOADING } from "../utils/actions";
+import API from "../utils/API";
+import { ADD_MEMBER } from "../utils/actions";
 
-export default class SignUp extends Component {
-    render() {
+
+
+function CreateMemberForm() {
+    const firstNameRef = useRef();
+    const lastNameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const [state, dispatch] = useStoreContext();
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      dispatch({ type: LOADING });
+      API.saveMember({
+        firstname: firstNameRef.current.value,
+        lastname: lastNameRef.current.value,
+        email: emailRef.current.value,
+        password: passwordRef.current.value
+      })
+        .then(result => {
+          dispatch({
+            type: ADD_MEMBER,
+            member: result.data
+          });
+        })
+        .catch(err => console.log(err));
+  
+      firstNameRef.current.value = "";
+      lastNameRef.current.value = "";
+      emailRef.current.value = "";
+      passwordRef.current.value = "";
+    };
+  
+// export default class SignUp extends Component {
+    // render() {
         return (
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input type="text" className="form-control" ref={firstNameRef} placeholder="First name" />
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input type="text" className="form-control" ref={lastNameRef} placeholder="Last name" />
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control" required ref={emailRef} placeholder="Enter email" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control" required ref={passwordRef}placeholder="Enter password" />
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <button type="submit" className="btn btn-primary btn-block" disabled={state.loading}>Sign Up</button>
                 <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
+                    Already registered <a href={"/sign-in"}>sign in?</a>
                 </p>
             </form>
         );
-    }
+    // }
 }
+
+export default CreateMemberForm;
