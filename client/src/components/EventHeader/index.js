b21b2d42a7f374d3bfe63d730ce65a5bee0c9d0e
TheMattyDGWU <themattydgwu@gmail.com>
2021-01-16 10:41:57 -0500

    refactor

--- client/src/components/EventHeader/index.js
+++ client/src/components/EventHeader/index.js
@@ -5,7 +5,7 @@ function EventHeader({ children }) {
   return (
     <div
       style={{ height: 125, clear: "both", paddingTop: 10, textAlign: "center" }}
-      className="page-header border border-secondary"
+      className="page-header border border-primary"
     >
       {children}
     </div>
