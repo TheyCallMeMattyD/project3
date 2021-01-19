fe1c9c61ff3f70a4348516a519d75f7489cf3a50
chefbrams <oldcitycatering@gmail.com>
2021-01-19 14:28:04 -0500

    used get route to get location info to pass through geocode function.

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,6 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { GoogleMap, LoadScript, MarkerClusterer, Marker } from '@react-google-maps/api';
-// const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+import Geocode from "react-geocode";
+import API from "../utils/API";
+import { useStoreContext } from "../utils/GlobalState";
+import { SET_CURRENT_POST} from "../utils/actions";
+
+
+  
+  
+
+
+
+function createKey(location) {
+  return location.lat + location.lng
+}
+
+function MapComponent(props) {
+  // props.match.params.id
+  console.log(props)
+  const [state, dispatch] = useStoreContext();
+  useEffect(() => {
+    API.getPost()
+      .then(res => dispatch({ type: SET_CURRENT_POST, post: res.data }))
+      .catch(err => console.log(err));
+     
+  }, []);
+
+  // const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+Geocode.setApiKey("AIzaSyBojLhxV3SQs5u5-LoByYaEmnJmgu8GCBk");
+Geocode.setLanguage("en");
+Geocode.setRegion("us");
+Geocode.enableDebug();
+Geocode.fromAddress(state.currentPost.location).then(
+  response => {
+   const lat = response.results[0].geometry.location.lat;
+   const lng = response.results[0].geometry.location.lng;
+   console.log(lat,lng, "LATS");
+    
+    console.log(response.results[0].geometry.location , "TEST");
+  },
+  error => {
+    console.error(error);
+  })
+  
+
+
+
 const containerStyle = {
   width: '400px',
   height: '400px'
@@ -12,29 +57,18 @@ const center = {
 };
 
 const locations = [{
-
+  
   lat: 38.900497,
   lng: -77.007507
-},
-{lat: 38.8719,
-lng: -77.0563}
-
-]
-function createKey(location) {
-  return location.lat + location.lng
 }
+  ];
 
-
-
-const onLoad =  marker => {
-    console.log( 'marker:', marker)
-  }
-
-
-function MyComponent() {
+  
+console.log(state, "this is state")
   return (
+    
     <LoadScript
-      googleMapsApiKey=""
+      googleMapsApiKey="AIzaSyBojLhxV3SQs5u5-LoByYaEmnJmgu8GCBk"
     >
       <GoogleMap
       id="marker-example"
@@ -59,4 +93,4 @@ function MyComponent() {
   )
 }
 
-export default React.memo(MyComponent)
+export default React.memo(MapComponent)
